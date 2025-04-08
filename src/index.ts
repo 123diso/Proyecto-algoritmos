7ad123fcf58600ec2ce4746ed7e52ea518dbef8d
123diso <d.f9821n@gmail.com>
2025-04-08 13:58:30 -0500

    Docs añadiendo codigo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 console.log("Hello, TypeScript!");
+
+import  Sidebar  from './Component/layout/sidebar';
+import SimpleNavbar from './Component/nav/searchicon';
+import ProfileComponent from './Component/start/smallprofile/smallprofile';
+import Root from './Component/root/Root';
+
+
+customElements.define('app-sidebar', Sidebar);
+customElements.define('simple-navbar', SimpleNavbar);
+customElements.define('profile-component', ProfileComponent);
+customElements.define("my-root", Root);
