571363a703fe7f498b8c01c3d000e559e97a9492
123diso <d.f9821n@gmail.com>
2025-04-10 11:49:46 -0500

    Añadiendo categorias

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import  Sidebar  from './Component/layout/sidebar';
 import SimpleNavbar from './Component/nav/searchicon';
 import ProfileComponent from './Component/start/smallprofile/smallprofile';
 import Root from './Component/root/Root';
+import CategoryCarousel from './Component/start/carousel/category';
 
 
 customElements.define('app-sidebar', Sidebar);
 customElements.define('simple-navbar', SimpleNavbar);
 customElements.define('profile-component', ProfileComponent);
 customElements.define("my-root", Root);
+customElements.define('category-carousel', CategoryCarousel);
